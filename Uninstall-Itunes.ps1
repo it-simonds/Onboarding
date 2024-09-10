953065ec6bb087546837497ff2baaba85a41438c
it-simonds <brian.roby@simonds.com.au>
2024-09-10 10:59:53 +1000

    Add files via upload

---
+++ Uninstall-Itunes.ps1
@@ -0,0 +1,25 @@
+# Find the installed iTunes program and its IdentifyingNumber (Product Code)
+$itunesApp = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name = 'iTunes'"
+
+# Check if iTunes is installed
+if ($itunesApp) {
+    # Uninstall iTunes using MSIExec and the IdentifyingNumber (Product Code)
+    $productCode = $itunesApp.IdentifyingNumber
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $productCode /quiet /norestart" -Wait
+    Write-Host "iTunes has been uninstalled successfully."
+} else {
+    Write-Host "iTunes is not installed on this machine."
+}
+
+
+# Find the iTunes app package from the Microsoft Store
+$itunesApp1 = Get-AppxPackage -Name "*AppleInc.iTunes*"
+
+# Check if the iTunes Microsoft Store app is installed
+if ($itunesApp1) {
+    # Uninstall iTunes
+    Remove-AppxPackage -Package $itunesApp.PackageFullName
+} else {
+}
+
+

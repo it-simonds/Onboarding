59510c26e343317a2dfdb320b865deec81b9732a
it-simonds <brian.roby@simonds.com.au>
2024-09-10 11:32:31 +1000

    Update Uninstall-Itunes.ps1

--- Uninstall-Itunes.ps1
+++ Uninstall-Itunes.ps1
@@ -6,9 +6,9 @@ if ($itunesApp) {
     # Uninstall iTunes using MSIExec and the IdentifyingNumber (Product Code)
     $productCode = $itunesApp.IdentifyingNumber
     Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $productCode /quiet /norestart" -Wait
-    Write-Host "iTunes has been uninstalled successfully."
+    
 } else {
-    Write-Host "iTunes is not installed on this machine."
+    
 }
 
 
@@ -18,10 +18,8 @@ $itunesApp1 = Get-AppxPackage -Name "*AppleInc.iTunes*"
 # Check if the iTunes Microsoft Store app is installed
 if ($itunesApp1) {
     # Uninstall iTunes
-    Remove-AppxPackage -Package $itunesApp1.PackageFullName
-    Write-Host "iTunes Microsoft Store app has been uninstalled successfully."
+    Remove-AppxPackage -Package $itunesApp.PackageFullName
 } else {
-    Write-Host "iTunes Microsoft Store app is not installed on this machine."
 }
 
 
